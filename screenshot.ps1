57c10453589aef220dc21d12ff75846e5bafa745
zlc1004 <91501777+zlc1004@users.noreply.github.com>
2024-06-04 17:53:28 -0700

    Update screenshot.ps1

--- screenshot.ps1
+++ screenshot.ps1
@@ -5,7 +5,8 @@ $Width = $Screen.Width
 $Height = $Screen.Height
 $Left = $Screen.Left
 $Top = $Screen.Top
-$bitmap = New-Object System.Drawing.Bitmap $Width, $Height--
+
+$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
 $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
 $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
 $stream = New-Object System.IO.MemoryStream
